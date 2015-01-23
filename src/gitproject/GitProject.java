a44d0cd7ab68b8e3d06a16f12b6baef10440d619
latifa-damouh <damouh.latifa@courrier.uqam.ca>
2015-01-22 21:38:14 -0500

    First Class

--- src/gitproject/GitProject.java
+++ src/gitproject/GitProject.java
@@ -17,6 +17,7 @@ public class GitProject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Word");
+        System.out.println("Premier Project");
     }
     
 }
