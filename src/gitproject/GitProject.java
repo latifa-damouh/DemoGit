45100f9282056a5e7b549a21f0696e26cd66c9d1
latifa-damouh <damouh.latifa@courrier.uqam.ca>
2015-01-22 21:13:17 -0500

    First Class

---
+++ src/gitproject/GitProject.java
@@ -0,0 +1,22 @@
+/*
+ Latifa Damouh 
+ DAML11538206
+ INF2015
+ */
+package gitproject;
+
+/**
+ *
+ * @author Latifa
+ */
+public class GitProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Word");
+    }
+    
+}
